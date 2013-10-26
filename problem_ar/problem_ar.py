dd7764b6b039839e3039ce2e2e9864715730016b
Andrew Blackton <ablackton@gmail.com>
2013-10-26 06:13:45 -0500

    Just the basics

--- problem_ar/problem_ar.py
+++ problem_ar/problem_ar.py
@@ -1,1 +1,24 @@
 ###
+import sys
+
+line1 = sys.stdin.readline().strip("\n");
+line2 = sys.stdin.readline().strip("\n");
+
+lstShowInfo = line1.split();
+lstSignatureShows = line2.split();
+
+nShowCount = lstShowInfo.pop(0);
+nSignatureShowCount = lstSignatureShows.pop(0);
+
+lstSignatureShows = lstSignatureShows[::2]
+nMinsOverlap = 0;
+
+#print(lstShowInfo);
+#print(lstSignatureShows);
+
+nCurrentSigTime = nSignatureShowCount[0]
+#for i in range(len(lstShowInfo)):
+    
+
+
+
