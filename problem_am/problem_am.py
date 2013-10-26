ed0850f0be81ac1bca42485fa3e22529083d9a50
vptarmigan <vicky.ptarmigan@gmail.com>
2013-10-26 03:11:00 -0500

    done yay

--- problem_am/problem_am.py
+++ problem_am/problem_am.py
@@ -16,10 +16,15 @@ for i in range(int(row)):
 
 #swappy thing
 swapped = []
-for i in bigTable:
-	swapped.insert(0,i)
+swapped = bigTable
+#for i in bigTable:
+#	swapped.insert(0,i)
 
-currentRow = 1  #start at row 1
+#print "actual data"
+#for i in swapped:
+	#print i
+
+currentRow = row -2  #start at row 1
 
 summedRisks = []
 lst = []
@@ -27,33 +32,33 @@ for i in range(col):
 	lst.append((i,(i,i)))
 summedRisks.append(lst)
 
-while currentRow < row:
+while currentRow >= 0 :
 	curRowMeat = []
 	for currentCol,risk in enumerate(swapped[currentRow]):
 		#edge cases
 		risks = []
 		if currentCol == 0:
-			risks.append((0,swapped[currentRow-1][0]))
-			risks.append((1,swapped[currentRow-1][1]))
+			risks.append((0,swapped[currentRow+1][0]))
+			risks.append((1,swapped[currentRow+1][1]))
 		elif currentCol == (col-1):
-			risks.append((col-1,swapped[currentRow-1][col-1]))
-			risks.append((col-2,swapped[currentRow-1][col-2]))
+			risks.append((col-1,swapped[currentRow+1][col-1]))
+			risks.append((col-2,swapped[currentRow+1][col-2]))
 		else:
-			risks.append((currentCol-1,swapped[currentRow-1][currentCol-1]))
-			risks.append((currentCol,swapped[currentRow-1][currentCol]))
-			risks.append((currentCol+1,swapped[currentRow-1][currentCol+1]))
+			risks.append((currentCol-1,swapped[currentRow+1][currentCol-1]))
+			risks.append((currentCol,swapped[currentRow+1][currentCol]))
+			risks.append((currentCol+1,swapped[currentRow+1][currentCol+1]))
 		smallestRiskCell=  min(risks, key=lambda item:item[1])
 		smallestRisk = risk + smallestRiskCell[1]
-		locPrevCell = (currentRow-1, smallestRiskCell[0])
+		locPrevCell = (currentRow+1, smallestRiskCell[0])
 		#rewrite with smallest risk
 		swapped[currentRow][currentCol] = smallestRisk
 		curRowMeat.append((smallestRisk, locPrevCell))
-	currentRow+=1
-	summedRisks.append(curRowMeat)
+	currentRow-=1
+	summedRisks.insert(0,curRowMeat)
 
 #print "MEAT MEAT" 
 #for i in summedRisks:
-#	print i
+	#print i
 
 #FIND THE MIN ROUTE :D :D
 
@@ -62,33 +67,37 @@ while currentRow < row:
 path = []
 
 topSmallestCol = 0
-smallest = summedRisks[-1][0][0]
-for index, value in enumerate(summedRisks[-1]):
+smallest = summedRisks[0][0][0]
+for index, value in enumerate(summedRisks[0]):
 	if value[0] < smallest:
 		smallest = value[0]
 		topSmallestCol =  index
 
-		
+#print "smallest " + str(smallest)
+#print "smallestCol " + str(topSmallestCol)
+	
+
 #smallestRiskCell = min(summedRisks[-1], key = lambda item:item[0])
-smallestRiskCell = (smallest, (row-1,topSmallestCol))
 #print "smallestRiskCell "  + str(smallestRiskCell)
-locPrev = smallestRiskCell[1]
 #print "locPrev " + str(locPrev)
+path.append((0,topSmallestCol)) #this is where we came from
+locPrev = summedRisks[0][topSmallestCol][1]
 path.append(locPrev)
 while True:
+	
 	climbingRow = locPrev[0]
 	nextCol = locPrev[1]
 	locPrev = summedRisks[climbingRow][nextCol][1]
 	#print "locPRev inloop " + str(locPrev)
 	path.append(locPrev)
-	if climbingRow == 1:
+	if climbingRow == row-2:
 		break
 
 #for i in path:
 #	print i
 
 meat = "Minimum risk path = "
-path.reverse()
+#path.reverse()
 for i in path:
 	thingy = "[" + str(i[0]) + "," + str(i[1]) + "]"
 	meat += thingy
